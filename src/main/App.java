2df33464ed852d033e6108b89bde02f0f6c3a33c
viktor <vikt.ovod@outlook.com>
2024-12-07 20:51:27 +0300

    возвращение к первоначальному сроку открытия магазина через 2 месяца
    (cherry picked from commit 189c04b576a7648aac82a7f806a3a3c47f3aa596)

--- src/main/App.java
+++ src/main/App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Открытие магазина ожидается через 2 месяца");
-        System.out.println("Открытие магазина ожидается через 4 дня");
-        System.out.println("[обновление завершено]");
-
     }
 }
